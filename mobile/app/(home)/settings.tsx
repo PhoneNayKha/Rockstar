22cd8d1927626bc1dac5377b2f728c214da672fc
phonenaykha-mhs <phonenaykha25.mhs@gmail.com>
2025-03-23 16:05:46 +0630

    update within class

---
+++ mobile/app/(home)/settings.tsx
@@ -0,0 +1,12 @@
+import { ThemeButton } from "@/components/theme-button";
+import { View } from "react-native";
+
+export default function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ gap: 10 }}>
+        <ThemeButton />
+      </View>
+    </View>
+  );
+}
